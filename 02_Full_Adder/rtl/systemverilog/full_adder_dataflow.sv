40509999c0202871fbf75eadd15b4635c5769e1f
Saikumar <kalisetty.saikumar7s@gmail.com>
2026-08-08 15:29:03 +0530

    feat: implement dataflow SystemVerilog full adder with testbench

---
+++ 02_Full_Adder/rtl/systemverilog/full_adder_dataflow.sv
@@ -0,0 +1,12 @@
+module full_adder_dataflow(
+    input logic A,
+    input logic B,
+    input logic Cin,
+    output logic Sum,
+    output logic Cout
+);
+
+assign Sum = A ^ B ^ Cin;
+assign Cout = (A & B) | (B & Cin) | (A & Cin);
+
+endmodule
