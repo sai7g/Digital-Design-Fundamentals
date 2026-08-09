8ff7783ce5c48a70aab09ea75e6e546cb67e8dfe
Saikumar <kalisetty.saikumar7s@gmail.com>
2026-08-09 10:37:44 +0530

    feat: implement dataflow SystemVerilog 1:16 demultiplexer with testbench

---
+++ 04_Demultiplexer/rtl/systemverilog/demux1to16_dataflow.sv
@@ -0,0 +1,24 @@
+module demux1to16_dataflow (
+    input  logic        din,
+    input  logic [3:0]  sel,
+    output logic [15:0] y
+);
+
+assign y[0]  = (~sel[3] & ~sel[2] & ~sel[1] & ~sel[0]) & din;
+assign y[1]  = (~sel[3] & ~sel[2] & ~sel[1] &  sel[0]) & din;
+assign y[2]  = (~sel[3] & ~sel[2] &  sel[1] & ~sel[0]) & din;
+assign y[3]  = (~sel[3] & ~sel[2] &  sel[1] &  sel[0]) & din;
+assign y[4]  = (~sel[3] &  sel[2] & ~sel[1] & ~sel[0]) & din;
+assign y[5]  = (~sel[3] &  sel[2] & ~sel[1] &  sel[0]) & din;
+assign y[6]  = (~sel[3] &  sel[2] &  sel[1] & ~sel[0]) & din;
+assign y[7]  = (~sel[3] &  sel[2] &  sel[1] &  sel[0]) & din;
+assign y[8]  = ( sel[3] & ~sel[2] & ~sel[1] & ~sel[0]) & din;
+assign y[9]  = ( sel[3] & ~sel[2] & ~sel[1] &  sel[0]) & din;
+assign y[10] = ( sel[3] & ~sel[2] &  sel[1] & ~sel[0]) & din;
+assign y[11] = ( sel[3] & ~sel[2] &  sel[1] &  sel[0]) & din;
+assign y[12] = ( sel[3] &  sel[2] & ~sel[1] & ~sel[0]) & din;
+assign y[13] = ( sel[3] &  sel[2] & ~sel[1] &  sel[0]) & din;
+assign y[14] = ( sel[3] &  sel[2] &  sel[1] & ~sel[0]) & din;
+assign y[15] = ( sel[3] &  sel[2] &  sel[1] &  sel[0]) & din;
+
+endmodule
