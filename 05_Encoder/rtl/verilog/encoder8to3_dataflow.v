3b3b90099897132cb2779fabc2bc140ea571811e
Saikumar <kalisetty.saikumar7s@gmail.com>
2026-08-19 20:55:51 +0530

    feat: implement dataflow Verilog 8:3 encoder with testbench

---
+++ 05_Encoder/rtl/verilog/encoder8to3_dataflow.v
@@ -0,0 +1,10 @@
+module encoder8to3_dataflow(
+    input [7:0] I,
+    output [2:0] Y
+);
+
+assign Y[0] = I[1] | I[3] | I[5] | I[7];
+assign Y[1] = I[2] | I[3] | I[6] | I[7];
+assign Y[2] = I[4] | I[5] | I[6] | I[7];
+
+endmodule
